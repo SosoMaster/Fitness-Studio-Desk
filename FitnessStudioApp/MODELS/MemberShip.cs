207602d79d63376b67efc37f89dacd40bc7291f1
Viktor5865 <viktormarinovski9@gmail.com>
2026-04-09 23:06:20 +0300

    Add:Bookingmodel,MemberShipmodel

---
+++ FitnessStudioApp/MODELS/MemberShip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessStudioApp.MODELS
+{
+    public class Membership
+    {
+        public int MembershipId { get; set; }  
+        public int ClientId { get; set; }      
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; }
+
+       
+        public Client Client { get; set; }
+    }
+}
