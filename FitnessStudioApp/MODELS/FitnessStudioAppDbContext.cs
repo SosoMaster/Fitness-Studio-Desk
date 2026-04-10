332e76c91d953813aae6cd87b5d792953726e985
Alexander Gatev <161458542+SosoMaster@users.noreply.github.com>
2026-04-10 11:19:38 +0300

    First relations

---
+++ FitnessStudioApp/MODELS/FitnessStudioAppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessStudioApp.MODELS
+{
+    public class FitnessStudioAppDbContext : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<Progress> Progresses { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=fitnessstudioapp.db");
+
+        }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(u =>
+            {
+                u.HasOne(u => u.Client)
+                .WithOne(c => c.User)
+                .HasForeignKey<Client>(c => c.UserId);
+
+                u.HasOne(u => u.Admin)
+                .WithOne(c => c.User)
+                .HasForeignKey<Admin>(c => c.UserId);
+
+                u.HasOne(u => u.Trainer)
+                .WithOne(c => c.User)
+                .HasForeignKey<Trainer>(c => c.UserId);
+            });
+
+            
+        }
+    }
+}
