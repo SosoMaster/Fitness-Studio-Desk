863b0d6e4561af919ed041b133f397ee98171a55
VesoGinNa <veselinchobanski@gmail.com>
2026-04-16 00:32:39 +0300

    Baserepository, ClientRepo, UserRepo and IService

--- FitnessStudioApp/REPOSITORY/Interfaces/IMembershipRepository.cs
+++ FitnessStudioApp/REPOSITORY/Interfaces/IMembershipRepository.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using FitnessStudioApp.MODELS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FitnessStudioApp.REPOSITORY;
+namespace FitnessStudioApp.REPOSITORY.Interfaces;
 
 public interface IMembershipRepository : IRepository<Membership>
 {
